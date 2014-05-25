623b5262abc0ea41bad34476553ebc48b295a901
yaoxinming <yaoxinming@gmail.com>
2014-05-25 19:55:57 +0800

    sdp parser

--- protocol/sdp.lua
+++ protocol/sdp.lua
@@ -1,4 +1,5 @@
 local lpeg = require "lpeg"
+local hive_lib = require "hive.hive_lib"
 local bnf = require "protocol.bnf"
 local R, S, V, P = lpeg.R, lpeg.S, lpeg.V, lpeg.P
 local C, Ct, Cmt, Cg, Cb, Cc = lpeg.C, lpeg.Ct, lpeg.Cmt, lpeg.Cg, lpeg.Cb, lpeg.Cc
@@ -33,7 +34,6 @@ local space = P' ' --l.space
 local alpha = l.alpha
 local alnum = l.alnum
 local digit = l.digit
-
 local safe = alnum + S'-./:?#$&*;=@[]^_{|}+~"' + P"'"  
 local email_safe = safe + space + tab
 local pos_digit = R"19"
@@ -47,7 +47,7 @@ local decimal_uchar = C(
 )
 local byte = P(1) - S("\0\r\n") 
 local byte_string =  byte^1--P"0x" * l.xdigit * l.xdigit
-local text = byte_string
+local text = safe^1
 local b1 = decimal_uchar - P'0' -- -P'127'
 local b4 = decimal_uchar - P'0'
 local ip4_address = b1 * P'.' * decimal_uchar * P'.' * decimal_uchar * P'.' * b4 
@@ -84,22 +84,23 @@ local connection_address = multicast_address + addr
 local sess_version = digit^1
 local sess_id = digit^1
 local att_value = byte_string
-local att_field = (safe - P":") ^1 
-local attribute =(att_field * P":" * att_value) + att_field
+local att_field = (safe - P":") ^1
+local attribute =(att_field * P":" * att_value) 
+   + att_field
 local port = digit^1
 local proto = (alnum + S"/")^1
 local fmt = alnum^1
 local media = alnum^1
 
 
-local proto_version = P"v=" * Cg(digit^1,"v") * crlf
+local proto_version = P"v=" * Cg(digit^1/tonumber,"v") * crlf
 local o_field = space_cg(username,"username") 
-   * space_cg(sess_id,"sess_id") 
-   * space_cg(sess_version,"sess_version")
+   * space_cg(sess_id/tonumber,"sess_id") 
+   * space_cg(sess_version/tonumber,"sess_version")
    * space_cg(nettype,"nettype")
    * space_cg(addrtype,"addrtype")
    * space_cg(addr,"addr")
-local origin_field =  P"o=" * Cg(Ct(o_field),"o") 
+local origin_field =  P"o=" * Cg(Ct(o_field) * crlf,"o") 
 local session_name_field = P"s=" *Cg(space_c(text) * crlf,"s")
 local information_field = P"i=" *Cg(space_c(text) * crlf,"i")
 local uri_field = P"u=" *Cg(space_c(uri) * crlf,"u")
@@ -112,23 +113,145 @@ local conn1 =
    *space_cg(addrtype,"addrtype")
    *space_cg(connection_address,"connection_address")
 local connection_field = P"c=" * Cg(Ct(conn1) * crlf,"c") 
-local bandwidth_field = P"b=" * Ct(space_cg(bwtype,"bwtype") * P":" * space_cg(bandwidth,"bandwidth")) * crlf,"b"
+local bandwidth_field = P"b=" * Ct(space_cg(bwtype,"bwtype") * P":" * space_cg(bandwidth/tonumber,"bandwidth")) * crlf
 local bandwidth_fields = Cg(Ct(bandwidth_field^1),"b")
 local time_field = P"t=" * Cg(space_c(any) * crlf,"t") --todo detail
 
 local key_field = P"k=" *Cg(space_c(text) * crlf,"k") --todo detail
-local media_field = P"m=" * space_cg(media,"media") * (space_cg(port,"port")*(P"/" * integer)^-1) * space_cg(proto,"proto") * space_cg(fmt,"fmt")^-1 * crlf,"m"
+local media_field = P"m=" * space_cg(media,"media") * (space_cg(port/tonumber,"port")*(P"/" * integer)^-1) * space_cg(proto,"proto") * space_cg(fmt,"fmt")^-1 * crlf,"m"
 
 local attribute_field = P"a=" * C(attribute) * crlf
 local attribute_fields = Cg(Ct(attribute_field ^1),"a")
+
+local information_field2 = P"i=" * space_c(text) * crlf
+local connection_field2 = P"c=" * Ct(conn1) * crlf
+local key_field2 = P"k=" * space_c(text) * crlf
 local media_description = 
-   Cg(Ct(media_field),"m") 
- --  * Cg(information_field^-1,"infomation")
---   * Cg(connection_field^0,"connection")
-   *Cg(Ct(bandwidth_field^1),"b")
---   *Cg(key_field^-1,"key")
-   *Cg(Ct(attribute_field^1),"a")
-local media_descriptions = Cg(Ct(media_description^1),"m")
+   Cg(Ct(media_field),"m")  
+   * Cg(information_field2^-1,"i")
+   * Cg(Ct(connection_field2^0),"c")
+   * Cg(Ct(bandwidth_field^1),"b")
+   * Cg(key_field2^-1,"k")
+   * Cg(Ct(attribute_field^1),"a")
+local media_descriptions = Ct(Cg(Ct(Ct(media_description)^1),"m")) --Cg(Ct(media_description^1),"m")
+
+local media_descriptions2 = Cg(Ct(Ct(media_description)^1),"m")
+local session_description = 
+   Ct(
+      proto_version
+	 * origin_field
+	 * session_name_field
+	 * information_field^-1
+	 * uri_field^-1
+	 * email_field^-1
+	 * connection_field^-1
+	 * bandwidth_fields
+	 * attribute_fields
+	 * media_descriptions2
+   )
+
+local req_meta = {}
+local build_field
+build_field = {
+   ["v"] = function(req,v)
+      table.insert(req,string.format("v=%d\r\n",v))
+   end,
+   ["o"] = function(req,o)
+      table.insert(req,string.format("o=%s %d %d %s %s %s \r\n",
+				      o.username,o.sess_id
+				      ,o.sess_version,o.nettype,o.addrtype
+				      ,o.addr
+				     )
+      )
+   end,
+   ["b"] = function(req,b)
+      local i
+      for i=1, #(b) do
+	 table.insert(req,string.format("b=%s:%d \r\n",b[i].bwtype,b[i].bandwidth))
+      end
+   end,
+   ["c"]= function(req,c)
+      table.insert(req,string.format("c=%s %s %s \r\n",c.nettype,c.addrtype,c.connection_address))
+   end,
+   ["s"] = function(req,s)
+      table.insert(req,string.format("s=%s\r\n",s))
+   end,
+   ["i"] = function(req,i)
+      table.insert(req,string.format("i=%s\r\n",i))
+   end,
+   ["a"] = function(req,a)
+      local i
+      for i = 1, #(a) do
+	 table.insert(req,string.format("a=%s",a[i]))
+      end
+   end,
+   ["media"] = function(req,media)
+      table.insert(req,string.format("m=%s %s  %s  %s\r\n",media.media,media.port,media.proto,media.fmt))
+   end,
+   ["m"] = function(req,m)
+      for i = 1 , #(m) do
+	 local media   = m[i].m 
+	 build_field["media"](req,media)
+	 if m[i].i and m[i].i ~= "" then --todo fixme why i has value
+	    build_field["i"](req,m[i].i)
+	 end
+	 if m[i].c then
+	    local j
+	    for j = 1, #(m[i].c) do
+	       build_field["c"](req,m[i].c[j])
+	    end
+	 end
+	 
+	 if m[i].b then
+	    build_field["b"](req,m[i].b)
+	 end
+	 
+	 if m[i].a then
+	    assert(type(m[i].a) == "table")
+	    build_field["a"](req,m[i].a)
+	 end
+      end
+   end
+}
+
+function sdp.build_req(sdp)
+   assert(sdp and type(sdp) == "table")
+   assert(sdp.o and sdp.s)
+   assert(sdp.m)
+   local data = { }
+   local k,v
+
+   if sdp.v then
+      build_field["v"](data,sdp.v)
+   end
+   build_field["o"](data,sdp.o)
+   build_field["s"](data,sdp.s)
+   
+
+   if sdp.c then
+      build_field["c"](data,sdp.c)
+   end
+   local i
+   if sdp.a then
+      build_field["a"](data,sdp.a)
+   end
+
+   assert(sdp.m,"must have media")
+   build_field["m"](data,sdp.m)
+   
+
+   return hive_lib.strjoin("\r\n",data).."\r\n\r\n"
+  
+end
+
+function sdp.parse(bin)
+   assert(bin and type(bin)=="string")
+   local req = session_description:match(bin)
+   if not req then
+      return false
+   end
+   return setmetatable(req,{__index = req_meta})
+end
 
 function sdp.bnf_test()
    local du1 = "254"
@@ -197,17 +320,20 @@ function sdp.bnf_test()
    assert(att.a[1] == "rtpmap:97 H264/90000")
    assert(att.a[2] == "bb:12")
 
-   local media ="m=video 0 qq 98\r\n"..
+   local media ="m=video 123 qq 98\r\n"..
+      "i=Info\r\n"..
+      "c=IN IP4 127.0.0.1\r\n"..
+      "c=IN IP6 127.0.0.1\r\n"..
       "b=AS:300\r\n"..
       "a=rtpmap:97 H264/90000\r\n"..
       "a=aesid:201\r\n"
 
    assert(att_value:match("mpeg4-esid"))
-   local media2 = "m=audio 0 qq 97\r\n"..
+   local media2 = "m=audio 22 qq 97\r\n"..
       "b=AS:300\r\n"..
       "a=rtpmap:97 H264/90000\r\n"..
-      "a=mpeg4-esid:201\r\n"..
-      "m=audio 0 qq 97\r\n"..
+      "a=mpeg4esid:201\r\n"..
+      "m=vedio 0 qq 97\r\n"..
       "b=AS:300\r\n"..
       "a=rtpmap:97 H264/90000\r\n"..
       "a=mpeg4esid:201\r\n"
@@ -215,129 +341,51 @@ function sdp.bnf_test()
    print(media_description:match(media),string.len(media))
    local mt = Ct(media_description):match(media)
    assert(mt.m.media=="video")
+   assert(mt.i == "Info")
+   assert(mt.c[1].nettype=="IN")
+   assert(mt.c[2].addrtype=="IP6")
+   assert(mt.i=="Info")
    assert(mt.a[2]=="aesid:201")
    print(mt.b[1].bandwidth)
    assert(mt.b[1].bwtype=="AS")
    
    assert(media_descriptions:match(media2))
 
-   local mtt = Ct(Cg(Ct(media_description^1,"m"))):match(media2)
-   print(media_descriptions:match(media2),string.len(media2))
---   assert(mtt.m)
-   print(mtt.a[1])
---   assert(mtt.m[1].media.media=="video")
-   
-end
+   local mtt = Ct(Cg(Ct(Ct(media_description)^1),"m")):match(media2)
+   local mtt2 = media_descriptions:match(media2)
+   print(mtt.m[2].m.media)
+   print(mtt.m[1].m.port)
+   assert(mtt.m[2].m.media=="vedio")
+   assert(mtt2.m[2].m.media=="vedio")
 
-sdp.bnf_test()
-
-local digits = digit^1
-local letter = R('az','AZ') + P'-'
-local alphanum = digit +  letter  + P'.' + P'_' 
-local name = (alphanum)^1
-local any = l.print^1
-
---todo fixme
-local text = name
-local uri = name
-local email = name
-local phone  = name
-local bwtype = name
-local attribute = (alphanum + S'@:=.,')^1
-function sdp.parse(str)
---rfc 2327 Appendix A
-   local proto_version = P"v=" * Cg(l.digit^1,"v") * crlf
-   local origin_field = P"o=" * Cg(Ct(sdp.space(C(text))^1) * crlf,"o") 
-   local session = P"s=" *Cg(sdp.space(C(text)) * crlf,"s")
-   local infomataion = P"i=" *Cg(sdp.space(C(text)) * crlf,"i")
-   local uri_field = P"u=" *Cg(sdp.space(C(uri)) * crlf,"u")
-   local email_field = P"e=" *sdp.space(C(email)) * crlf
-   local email_fields = Cg(Ct(email_field ^1),"e")
-   local phone_field = P"p=" *sdp.space(C(phone)) * crlf
-   local phone_fields = Cg(Ct(phone_field ^1),"p")
-   local conn_field = P"c=" * Cg(Ct(sdp.space(C(text))^1) * crlf,"c") 
-   local band_width = P"b=" * Cg(Ct(sdp.space(C(bwtype)) * ":" * sdp.space(C(digits))) * crlf,"b")
-   local time_fields = P"t=" * Cg(sdp.space(C(safe)) * crlf,"t") --todo detail
-   local key_field = P"k=" *Cg(sdp.space(C(text)) * crlf,"k") --todo detail
-
-   local attr_field = P"a=" * C(attribute) * crlf
-   local attr_fields = Cg(Ct(attr_field ^1),"a")
---   local media_field = P"m=" * sdp.space(media) * sdp.space(C(media_port)) * sdp.space(C(text)) * sdp.space(C(text))
-   local media_field = P"m=" * Cg(Ct(sdp.space(C(text))^1) * crlf,"m") 
-   local media_desc = media_field
-      --* infomataion^0 * conn_field^0 * band_width * key_field^0 * attr_fields^0
-   local media_descs = Cg(Ct(media_desc^1),"media")
-   local sdp = Ct(proto_version 
-		     * origin_field
-		     * session
-		     * infomataion^-1
-		     * uri_field^-1
-		     * email_fields^-1
-		     * phone_fields^-1
-		     * conn_field
-		     * band_width^-1
-		     * time_field
-		     * key_field^-1
-		     * attr_fields	 
-		     * media_descs
-		  ,rawset)
-   local r = sdp:match(str)
-   return r
-end
-
-
-function sdp.test1()
-   local str = "v=1\r\n"..
-      "o=test 931665148 2 IN IP4 127.0.0.0\r\n"..
+   local session = 
+      "v=1\r\n"..
+      "o=test 931665148 2 IN IP4 192.0.0.1\r\n"..
       "s=QuickTime\r\n"..
-      "e=aa\r\n"..
-      "e=bb\r\n"..
-      "p=138\r\n"..
-      "p=139\r\n"..
       "c=IN IP4 127.0.0.1\r\n"..
       "b=av:30\r\n"..
-      "t=0 0\r\n"..
-      "a=x-qt-text-an@:railsconf\r\n"..
       "a=range:npt=\r\n"..
       "a=isma-compliance:2,2.0,2\r\n"..
-      "m=audio 0 RTP/AVP 96\r\n"..
-      "b=AS:8\r\n"..
-      "a=rtpmap:96 mpeg4-generic/8000/1\r\n"..
-      "a=fmtp:96 profile-level-id=15;mode=AAC-hbr;sizelength=13;indexlength=3;indexdeltalength=3;config=1588\r\n"..
-      "a=mpeg4-esid:101\r\n"..
-      "a=control:trackid=1\r\n"..
-      "m=video 0 qq 97\r\n"..
-      "b=AS:300\r\n"..
-      "a=rtpmap:97 H264/90000\r\n"..
-      "a=fmtp:97 packetization-mode=1;profile-level-id=4D400C;sprop-parameter-sets=J01ADKkYUI/LgDUGAQa2wrXvfAQ=,KN4JF6A=\r\n"..
-      "a=mpeg4-esid:201\r\n"..
-      "a=cliprect:0,0,120,160\r\n"..
-      "a=framesize:97 160-120\r\n"..
-      "a=control:trackid=2\r\n"
-   print(str)
-   local t = sdp.parse(str)
-
-   print(t["v"])
-   print(t["o"][1])
-   print(t["s"])
-   print(t["i"])
-   print(t["u"])
-   print(t["e"][1])
-   print(t["e"][2])
-
-   print(t["p"][1])
-   print(t["p"][2])
+      "" .. media2
    
-   print(t["c"][1])
-   print(t["c"][2])
-   print(t["c"][3])
+   local st = session_description:match(session)
+   assert(st)
+   assert(st.v == 1)
+   assert(st.s == "QuickTime")
+   assert(st.c.nettype == "IN")
+   assert(st.c.addrtype == "IP4")
+   assert(st.c.connection_address == "127.0.0.1")
+   print(st.b[1].bandwidth)
+   assert(st.b[1].bandwidth == "30")
+   assert(st.m[1].m.media == "audio")
+   assert(st.m[2].m.media == "vedio")
    
-   print(t["b"][1])
-   print(t["b"][2])
-   print(t["t"])
-   print(t["k"])
-   print(t["a"][1])
-   print(t["a"][2])
+   print("xxxxx",#(st.m[1].a))
+   return st
 end
+
+local t =sdp.bnf_test()
+local t1 = sdp.build_req(t)
+print(t1)
 --sdp.test1()
 return sdp
